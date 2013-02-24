d8c0a2a5868f900b55896cfaece849294052c5f3
kimrunarsh <kimrunarsh@localhost>
2013-02-24 23:00:44 +0100

    Fungerande feedbacksystem

--- feedback.php
+++ feedback.php
@@ -1,32 +1,69 @@
+
 <?php
 include 'header.php';
 include 'connect.php';
+include("navBar.php");
+#foreach($_POST as $key => $value)
+#  {
+#    echo "$key eaquals $value <br>";
+#  }
+?>
 
-foreach($_POST as $key => $value)
-  {
-    echo "$key eaquals $value <br>";
-  }
-
+<div class="hero-unit">
+<center>
+<?php
 $batch=$_POST["batch"];
-$batch=$_POST["bottle"];
-$commenter=$_POST["commenter"];
-$comment=$_POST["comment"];
+$bottle=$_POST["bottle"];
+$commenter=clean($_POST["commenter"]);
+$comment=clean($_POST["comment"]);
+$member_id=clean($_POST["member_id"]);
 
-$commenterQuery="INSERT INTO commenters (commenterName)
-VALUES ('$commenter')";
-
-$sql="INSERT INTO comments (FirstName, LastName, Age)
-VALUES
-('$_POST[firstname]','$_POST[lastname]','$_POST[age]')";
-
-if (!mysql_query($sql,$con))
+if(isset($_POST["commenter"]))
   {
-  die('Error: ' . mysql_error());
+    $result = mysql_query("SELECT * FROM commenters WHERE commenterName='$commenter'");
+    trigger_error("SELECT * FROM commenters WHERE commenterName='$commenter'");
+    if (mysql_num_rows($result) == 0 )
+      {	
+	$commenterQuery="INSERT INTO commenters (commenterName) VALUES ('$commenter')";
+	$result = mysql_query($commenterQuery);
+      }
+     $result = mysql_query("SELECT * FROM commenters WHERE commenterName='$commenter'");
+    $resultArray = mysql_fetch_array($result);
+    $commenterId = $resultArray['commenterId'];
+    $commentQuery="INSERT INTO comments (commentText, commenterId, batchNumber, bottleNumber) VALUES ('$comment','$commenterId','$batch','bottle')";
+    $insertComment = mysql_query($commentQuery);
+    if ($insertComment == FALSE)
+      {
+	die('Could not insert comment into database: ' . mysql_error());
+      }
+    else
+      {
+	echo "Comment added!";
+      }
   }
-echo "1 record added";
 
+if(isset($_POST["member_id"]))
+  {
+    $commentQuery="INSERT INTO comments (commentText, member_id, batchNumber, bottleNumber) VALUES ('$comment','$member_id','$batch','bottle')";
+    $result = mysql_query($commentQuery);
+  }
+
+
+?>
+</div>
+<?php
 mysql_close($con);
+
 
 include 'footer.php';
 
+function clean($str){
+  $str = @trim($str);
+  if(get_magic_quotes_gpc()) 
+    {
+      $str = stripslashes($str);
+    }
+  return mysql_real_escape_string($str);
+}
+	
 ?>
--- heroButtons.php
+++ heroButtons.php
@@ -6,26 +6,37 @@
 	  <input name="batch" type="hidden" value="<?php if(isset($_GET['batch'])) echo htmlspecialchars($_GET['batch'], ENT_QUOTES); ?>">
 	  <input name="bottle" type="hidden" value="<?php if(isset($_GET['bottle'])) echo htmlspecialchars($_GET['bottle'], ENT_QUOTES); ?>"> 
 
-		<!-- Button to trigger modal -->
-    <a href="#myModal" role="button" class="btn btn-large" data-toggle="modal">Leave message</a>
+	  <!-- Button to trigger modal -->
+		<a href="#myModal" role="button" class="btn btn-large" data-toggle="modal">Leave message</a>
     
-    <!-- Modal -->
+		<!-- Modal -->
     <div id="myModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
       <div class="modal-header">
 	<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
 	<h3 id="myModalLabel">Feedback form</h3>
+      </div>
+      <div class="modal-body">
+      <fieldset>
+	<legend>Tell us something</legend>
+	<label>Go on..</label>
+	<?php
+	  if(isset($_SESSION['SESS_MEMBER_ID']))
+	    {
+	      echo 'Posting as <i>'.$_SESSION['SESS_FIRST_NAME'].'.</i> If you want to post under another name, please log out.';
+	      echo  '<input name="member_id" type="hidden" value='.$_SESSION["SESS_MEMBER_ID"].'>';
+	    }
+	  else
+	    {
+	      echo 'Name: <input type="text" name="commenter" placeholder="Put your name in this box."><br>';
+	    }
+	?>
+	<div>
+	 Comment: <textarea name="comment" rows="5" cols="10" ></textarea>
+	</div>
+	<span class="help-block">Your feedback is the most important thing in the world to us.</span>
+      </fieldset>
+      </div>
+    <div class="modal-footer">
+      <button type="submit" class="btn btn-primary">Submit</button>
     </div>
-    <div class="modal-body">
-	<fieldset>
-	  <legend>Tell us something</legend>
-	  <label>Go on..</label>
-	  <input type="text" name="commenter">
-	  <textarea placeholder="Type your comment about the beer here." name="comment" rows="5" cols="10">
-	</textarea>
-	    <span class="help-block">Your feedback is important to us.</span>
-    </fieldset>
-</div>
-<div class="modal-footer">
-  <button type="submit" class="btn btn-primary">Submit</button>
-</div>
-</form>
+  </form>
