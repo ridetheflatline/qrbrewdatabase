b73a1903cff0c31083f02a75a94a76055a93b12d
droidfinn <droidfinn@localhost>
2013-03-06 22:30:09 +0100

    Account page now shows user values, update button doesnt do anything yet.

--- account.php
+++ account.php
@@ -1,45 +1,54 @@
 <?php
 	include("header.php");
 	include("navBar.php");
-	$beerDb = mysql_connect("localhost","beer","cerveza");
-	if (!$beerDb)
+	$con = mysql_connect("localhost","beer","cerveza");
+	if (!$con)
 		die('Could not connect: ' . mysql_error());
 
+ 	mysql_select_db("beer", $con);	
+	$beerDb = mysql_select_db('beer', $con);
+	if (!$beerDb)
+    die ('Can\'t open db : ' . mysql_error());
+
 	if(isset($_SESSION['SESS_MEMBER_ID']))
-		echo '<p>ID: ' .$_SESSION['SESS_MEMBER_ID']. '</p>';
+		$member_id = $_SESSION['SESS_MEMBER_ID'];
 	else
 		header("location: message.php?message=NotLoggedIn");
+
+	$memberResults = mysql_query("SELECT * from members where member_id=$member_id");
+	if(!$memberResults)
+		die ('Mysql query failed! How did that happen? : ' . mysql_error());
 ?>
-  <div class="hero-unit">
-    <center>
-      <h2>User account</h2>
-      <p>Your account information
-		</center>
-	</div>
+<div class="hero-unit">
+	<center>
+		<h2>User account</h2>
+		<p>Your account information
+	</center>
+</div>
 <div class="span6 offset4">
 <form class="form-horizontal">
 	<div class="control-group">
 		<label class="control-label" for="inputFirstName">First name</label>
 		<div class="controls">
-			<input type="text" id="inputFirstName" placeholder="Load from sql..">
+			<input type="text" id="inputFirstName" value=<?php echo "\"" .mysql_result($memberResults,0,"firstname"). "\""; ?>>
 		</div>
 	</div>
 	<div class="control-group">
 		<label class="control-label" for="inputLastName">Last name</label>
 		<div class="controls">
-			<input type="text" id="inputLastName" placeholder="Load from sql..">
+			<input type="text" id="inputLastName" value=<?php echo "\"" .mysql_result($memberResults,0,"lastname"). "\""; ?>>
 		</div>
 	</div>
 	<div class="control-group">
 		<label class="control-label" for="inputLogin">Login</label>
 		<div class="controls">
-			<input type="text" id="inputLogin" placeholder="Load from sql..">
+			<input type="text" id="inputLogin" value=<?php echo "\"" .mysql_result($memberResults,0,"login"). "\""; ?>>
 		</div>
 	</div>
 	<div class="control-group">
 		<label class="control-label" for="inputEmail">Email</label>
 		<div class="controls">
-			<input type="text" id="inputEmail" placeholder="Load from sql..">
+			<input type="text" id="inputEmail" value=<?php echo "\"" .mysql_result($memberResults,0,"email"). "\""; ?>>
 		</div>
 	</div>
 	<div class="control-group">
