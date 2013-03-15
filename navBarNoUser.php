b14a3ae70c6e82294f1acd25bfd81877b83bc425
droidfinn <droidfinn@localhost>
2013-03-15 17:24:05 +0100

    Added forgot password feature. TODO: Change password feature. Will update SQL later.

--- navBarNoUser.php
+++ navBarNoUser.php
@@ -3,8 +3,10 @@
 	<li><p></p></li>
 	<li><a href="createAccount.php" type="submit" name="Submit" value="createAccount">Create account</a></li>
 	<li class="divider"></li>
-<form id="loginForm" name="loginForm" method="post" action="Login/login.php">
+<form style="display: inline" id="loginForm" name="loginForm" method="post" action="Login/login.php">
 	<li><input name="login" type="text" class="textfield input-small" id="login" placeholder="Username"/></li>
 	<li><input name="password" type="password" class="textfield input-small" id="password" placeholder="Password" /></li>
 	<li><a href="#" onclick="$(this).closest('form').submit()">Login</a></li>
 </form>
+<li class="divider"></li>
+<li><a href="forgotPassword.php" type="submit" name="Submit" value="forgotPass">Forgot password?</a></li>
